a2638d4a5a51ce6ce952f856ff1976b77a592330
Lewis Daveriel Masallo <lewismasallo@outlook.com>
2023-11-27 13:18:34 +0800

    some functionalities

--- database.py
+++ database.py
@@ -1,18 +1,30 @@
-import config
 from tkinter import messagebox as mb
 import mysql.connector as mc
 from mysql.connector import errorcode
 
+# CONFIGURATIONS
+
+host = "localhost"
+port = "3306"
+database = "db_wst_project"
+user = "root"
+password = ""
+
+# database tables
+table_employee = "tbl_employee"
+employee_columns = ["id", "username", "password", "position", "isDeleted"]
+table_franchise = "tbl_franchise"
+franchise_columns = ["id", "operator_name", "driver_name", "body_number", "plate_number", "license_number", "share_capital", "isDeleted"]
+
+
 # DATABASE CONNECTIONS
-cols_employee = tuple(config.employee_columns[:-1])
-cols_franchise = tuple(config.franchise_columns[:-1])
+cols_employee = tuple(employee_columns[:-1])
+cols_franchise = tuple(franchise_columns[:-1])
 
 
 def get_connection() -> mc.MySQLConnection:
     try:
-        conn = mc.connect(host=config.host, port=config.port,
-                          database=config.database, user=config.user,
-                          password=config.password)
+        conn = mc.connect(host=host, port=port, database=database, user=user, password=password)
         return conn
     except mc.Error as err:  # Error Handling
         mb.showerror("Error", err)
@@ -24,8 +36,7 @@ def login(username: str, password: str):
         conn = get_connection()
         cursor = conn.cursor()
 
-        query = f"SELECT * FROM {
-            config.table_employee} WHERE username = %s AND password = %s AND isDeleted = 0;"
+        query = f"SELECT * FROM {table_employee} WHERE username = %s AND password = %s AND isDeleted = 0;"
         cursor.execute(query, (username, password))
 
         result = cursor.fetchone()
@@ -43,7 +54,7 @@ def list_franchise():
         conn = get_connection()
         cursor = conn.cursor()
 
-        query = f"SELECT * FROM {config.table_franchise} WHERE isDeleted = 0"
+        query = f"SELECT * FROM {table_franchise} WHERE isDeleted = 0"
         cursor.execute(query)
 
         result = cursor.fetchall()
--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import database as db
 
 
 def __main__():
-    # login = Login(tk.Tk())
-    # login.root.mainloop()
+    login = Login(tk.Tk())
+    login.root.mainloop()
     # bypass -> for testing purposes
-    main = Main(tk.Tk(), db.login("admin", "admin"))
+    # main = Main(tk.Tk(), db.login("admin", "admin"))
     # for faster debugging
-    main.root.bind("<FocusOut>", lambda event: event.widget.destroy())
-    main.root.mainloop()
+    # root.bind("<FocusOut>", lambda event: event.widget.destroy())
+    # main.root.mainloop()
 
 
 class Login:
@@ -55,68 +55,99 @@ class Login:
 
 class Main:
     def __init__(self, root: tk.Tk, employee: tuple):
-        # init the login attributes
+        # init the attributes
         self.root = root
         self.root.title("Main")
-        self.root.geometry("600x500")
+        # self.root.geometry("600x500")
         self.username = employee[1]
         self.position = employee[3]
+
+        self.selected_franchise = None
 
         # label for position
         tk.Label(self.root, text=f"USER:   {self.username}  ( {self.position} )").grid(
             column=0, row=0, sticky=tk.W)
 
         # innergrid -> Franchise Module
-        grid_franchise = ttk.LabelFrame(self.root, text="Franchise Table")
+        self.grid_franchise = ttk.LabelFrame(self.root, text="Franchise Table")
 
-        table_franchise = ttk.Treeview(grid_franchise, style="Treeview")
+        self.table_franchise = ttk.Treeview(self.grid_franchise, style="Treeview", height=5)
+
+        # Add Franchise Buttons
+        tk.Button(self.grid_franchise, text="ADD NEW", width=10).grid(column=1, row=0, sticky=tk.NSEW, padx=5, pady=[0, 10])
 
         # list franchise
-        table_franchise["columns"] = db.cols_franchise
+        self.table_franchise["columns"] = db.cols_franchise
 
-        self.column_design(table_franchise,
-                           "#0", "", width=5)
-        self.column_design(table_franchise,
-                           db.cols_franchise[0], "ID", width=5)
-        self.column_design(table_franchise,
-                           db.cols_franchise[1], "OPERATOR", width=50)
-        self.column_design(table_franchise,
-                           db.cols_franchise[2], "DRIVER", width=50)
-        self.column_design(table_franchise,
-                           db.cols_franchise[3], "BODY #", width=30)
-        self.column_design(table_franchise,
-                           db.cols_franchise[4], "PLATE #", width=30)
-        self.column_design(table_franchise,
-                           db.cols_franchise[5], "LICENSE #", width=50)
-        self.column_design(table_franchise,
-                           db.cols_franchise[6], "SHARES", width=30)
+        self.column_design(self.table_franchise, "#0", "#", width=5)
+        self.column_design(self.table_franchise, db.cols_franchise[0], "ID", width=50)
+        self.column_design(self.table_franchise, db.cols_franchise[1], "OPERATOR", width=100)
+        self.column_design(self.table_franchise, db.cols_franchise[2], "DRIVER", width=100)
+        self.column_design(self.table_franchise, db.cols_franchise[3], "BODY #", width=50)
+        self.column_design(self.table_franchise, db.cols_franchise[4], "PLATE #", width=80)
+        self.column_design(self.table_franchise, db.cols_franchise[5], "LICENSE #", width=80)
+        self.column_design(self.table_franchise, db.cols_franchise[6], "SHARES", width=100)
 
-        self.populate_list(table_franchise, db.list_franchise())
+        self.populate_list(self.table_franchise, db.list_franchise())
+        self.table_franchise.bind("<<TreeviewSelect>>", self.on_treeview_select)
 
-        # Franchise Buttons
-        tk.Button(grid_franchise, text="INSERT", width=10).grid(
-            column=0, row=1, sticky=tk.NSEW, padx=5, pady=[0, 10])
-        tk.Button(grid_franchise, text="UPDATE", width=10).grid(
-            column=1, row=1, sticky=tk.NSEW, padx=5, pady=[0, 10])
-        tk.Button(grid_franchise, text="DELETE", width=10).grid(
-            column=2, row=1, sticky=tk.NSEW, padx=5, pady=[0, 10])
+        # franchise information -> hidden first
+        self.franchise_information = tk.Frame(self.grid_franchise)
+        self.franchise_information.columnconfigure(0, weight=1)
+        self.franchise_information.columnconfigure(1, weight=2)
+        self.franchise_information.columnconfigure(2, weight=1)
+        self.franchise_information.columnconfigure(3, weight=2)
+        self.franchise_information.columnconfigure(4, weight=1)
+
+        self.franchise_information.rowconfigure(0, weight=1)
+        self.franchise_information.rowconfigure(1, weight=1)
+        self.franchise_information.rowconfigure(2, weight=1)
+        self.franchise_information.rowconfigure(3, weight=1)
+        self.franchise_information.rowconfigure(4, weight=1)
+
+        # franchise information -> buttons
+        tk.Button(self.franchise_information, text="UPDATE", width=10).grid(column=4, row=0, sticky=tk.NSEW, padx=5, pady=[0, 10])
+        tk.Button(self.franchise_information, text="DELETE", width=10).grid(column=4, row=1, sticky=tk.NSEW, padx=5, pady=[0, 10])
+        tk.Button(self.franchise_information, text="PAYMENT", width=10).grid(column=4, row=2, sticky=tk.NSEW, padx=5, pady=[0, 10])
+        tk.Button(self.franchise_information, text="GENERATE ID", width=10).grid(column=4, row=3, sticky=tk.NSEW, padx=5, pady=[0, 10])
+        tk.Button(self.franchise_information, text="FOLD", width=10, command=self.fold).grid(column=4, row=4, sticky=tk.NSEW, padx=5, pady=[0, 10])
+
+        # franchise inputs
+        tk.Label(self.franchise_information, text="ID: ").grid(column=0, row=0, sticky=tk.E)
+        self.E_id = tk.Entry(self.franchise_information, relief=tk.GROOVE, bd=2)
+        self.E_id.grid(column=1, row=0, sticky="NWE", padx=10, pady=5, ipadx=2, ipady=2)
+        tk.Label(self.franchise_information, text="BODY #: ").grid(column=2, row=0, sticky=tk.E)
+        self.E_bodynum = tk.Entry(self.franchise_information, relief=tk.GROOVE, bd=2)
+        self.E_bodynum.grid(column=3, row=0, sticky="NWE", padx=10, pady=5, ipadx=2, ipady=2)
+
+        tk.Label(self.franchise_information, text="OPERATOR: ").grid(column=0, row=1, sticky=tk.E)
+        self.E_operator = tk.Entry(self.franchise_information, relief=tk.GROOVE, bd=2)
+        self.E_operator.grid(column=1, row=1, sticky="NWE", padx=10, pady=5, columnspan=3, ipadx=2, ipady=2)
+        tk.Label(self.franchise_information, text="DRIVER: ").grid(column=0, row=2, sticky=tk.E)
+        self.E_driver = tk.Entry(self.franchise_information, relief=tk.GROOVE, bd=2)
+        self.E_driver.grid(column=1, row=2, sticky="NWE", padx=10, pady=5, columnspan=3, ipadx=2, ipady=2)
+
+        tk.Label(self.franchise_information, text="PLATE #: ").grid(column=0, row=3, sticky=tk.E)
+        self.E_platenum = tk.Entry(self.franchise_information, relief=tk.GROOVE, bd=2)
+        self.E_platenum.grid(column=1, row=3, sticky="NWE", padx=10, pady=5, ipadx=2, ipady=2)
+        tk.Label(self.franchise_information, text="LICENSE #: ").grid(column=2, row=3, sticky=tk.E)
+        self.E_license = tk.Entry(self.franchise_information, relief=tk.GROOVE, bd=2)
+        self.E_license.grid(column=3, row=3, sticky="NWE", padx=10, pady=5, ipadx=2, ipady=2)
 
         # grid connections
-        table_franchise.grid(column=0, row=0, sticky=tk.NSEW,
-                             padx=5, pady=5, columnspan=3)
+        self.table_franchise.grid(column=0, row=1, sticky=tk.NSEW, padx=5, pady=5, columnspan=3)
 
-        grid_franchise.grid(column=0, row=1, padx=10, pady=10, sticky="NEW")
-        grid_franchise.columnconfigure(0, weight=1)
-        grid_franchise.columnconfigure(1, weight=1)
-        grid_franchise.columnconfigure(2, weight=1)
+        self.grid_franchise.grid(column=0, row=1, padx=10, pady=10, sticky=tk.NSEW)
+        self.grid_franchise.columnconfigure(0, weight=5)
+        self.grid_franchise.columnconfigure(1, weight=1)
 
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(1, weight=1)
 
         # styles
         style = ttk.Style()
-        style.configure("Treeview", rowheight=14, font=('Arial', 8))
-        style.configure("Treeview.Heading", font=('Arial', 8, 'bold'))
+        style.configure("Treeview", rowheight=14, font=("Arial", 8))
+        style.configure("Treeview.Heading", font=("Arial", 8, "bold"))
 
     # populate the selected tree with the list
     def populate_list(self, tree: ttk.Treeview, list: tuple):
@@ -127,6 +158,31 @@ class Main:
     def column_design(self, tree: ttk.Treeview, header: str, title: str, width=30, **kwargs):
         tree.heading(header, text=title)
         tree.column(header, width=width, **kwargs)
+
+    # selected treeview binding
+    def on_treeview_select(self, event):
+        self.franchise_information.grid(column=0, row=2, sticky=tk.NSEW, padx=5, pady=5, columnspan=2)
+        selected_item = self.table_franchise.selection()[0]
+        self.selected_franchise = self.table_franchise.item(selected_item, "values")
+
+        # clear input
+        self.E_id.delete(0, tk.END)
+        self.E_operator.delete(0, tk.END)
+        self.E_driver.delete(0, tk.END)
+        self.E_bodynum.delete(0, tk.END)
+        self.E_platenum.delete(0, tk.END)
+        self.E_license.delete(0, tk.END)
+
+        # populate input
+        self.E_id.insert(0, self.selected_franchise[0])
+        self.E_operator.insert(0, self.selected_franchise[1])
+        self.E_driver.insert(0, self.selected_franchise[2])
+        self.E_bodynum.insert(0, self.selected_franchise[3])
+        self.E_platenum.insert(0, self.selected_franchise[4])
+        self.E_license.insert(0, self.selected_franchise[5])
+
+    def fold(self):
+        self.franchise_information.grid_forget()
 
 
 # to initialize all the above code before execution
