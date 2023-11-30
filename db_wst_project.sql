36e8a5ef5ad4701a77272a68d137263823c0fdfa
Lewis Daveriel Masallo <lewismasallo@outlook.com>
2023-11-30 13:09:57 +0800

    added database

---
+++ db_wst_project.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2023 at 06:09 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_wst_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_employee`
+--
+
+CREATE TABLE `tbl_employee` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `position` varchar(100) NOT NULL,
+  `isDeleted` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_employee`
+--
+
+INSERT INTO `tbl_employee` (`id`, `username`, `password`, `position`, `isDeleted`) VALUES
+(1, 'admin', 'admin', 'General Manager', 0),
+(2, 'lewis', 'lewis', 'Secretary', 0),
+(3, 'arwyn', 'arwyn', 'Treasurer', 0),
+(4, 'diero', 'diero', 'Bookkeeper', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_franchise`
+--
+
+CREATE TABLE `tbl_franchise` (
+  `id` int(11) NOT NULL,
+  `driver_name` varchar(100) NOT NULL,
+  `body_number` varchar(4) NOT NULL,
+  `plate_number` varchar(20) NOT NULL,
+  `license_number` varchar(20) NOT NULL,
+  `isDeleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_franchise`
+--
+
+INSERT INTO `tbl_franchise` (`id`, `driver_name`, `body_number`, `plate_number`, `license_number`, `isDeleted`) VALUES
+(1, 'Pedro', '111', 'LS-666', '060602', 0),
+(2, 'Maria', '222', 'LS-667', '060601', 0),
+(3, 'Jose', '3333', 'LS-668', '060600', 0),
+(4, 'Max', '555', 'LS-670', '060698', 1),
+(5, 'Vince', '444', 'LS-669', '060699', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_employee`
+--
+ALTER TABLE `tbl_employee`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_franchise`
+--
+ALTER TABLE `tbl_franchise`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_employee`
+--
+ALTER TABLE `tbl_employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `tbl_franchise`
+--
+ALTER TABLE `tbl_franchise`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
